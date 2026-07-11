95079a4c5c14083946e20605eecbbf2c8a29c1ae
alesayi1 <alesayi823@gmail.com>
2026-07-11 13:50:58 +0000

    add

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import React from "react";
+
+export default function Layout({children}: {children: React.ReactNode}) {
+  return (
+    <>
+    <Header/>
+    {children}
+    <Footer/>
+    </>
+  )
+}
